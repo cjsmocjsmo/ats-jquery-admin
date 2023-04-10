3ec923a31e5ab7f845b30e6198c44f68ce41b45a
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-04-10 15:26:22 -0700

    add deploy script

---
+++ CLOUD_RUN_DEPLOY.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION="ats-admin-jq-httpd:0.0.1";
+
+docker build -t $VERSION .;
+docker run -d -p 8080:80 $VERSION
+
+##################################################
+
+# VERSION="gcr.io/atsgo-340504/ats-admin-jq-httpd:0.0.1";
+
+# npm run build;
+# docker build -t $VERSION .;
+# docker push $VERSION
