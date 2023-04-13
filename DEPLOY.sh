a0360933e2234aefe5e2e22b02c457e492a226ac
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-04-13 11:52:11 -0700

    deploy prep

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-VERSION="ats-admin-jq-httpd:0.0.1";
+# VERSION="ats-admin-jq-httpd:0.0.1";
 
-docker build -t $VERSION .;
-docker run -d -p 8080:80 $VERSION
+# docker build -t $VERSION .;
+# docker run -d -p 8080:80 $VERSION
 
 ##################################################
 
-# VERSION="gcr.io/atsgo-340504/ats-admin-jq-httpd:0.0.1";
+VERSION="gcr.io/atsgo-340504/ats-admin-jq364-httpd:0.0.1";
 
-# npm run build;
-# docker build -t $VERSION .;
-# docker push $VERSION
+npm run build;
+docker build -t $VERSION .;
+docker push $VERSION
